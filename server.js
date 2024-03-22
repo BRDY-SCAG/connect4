48d1ea7efb8f81b547a5f0220d52762c5d0ae7a1
BRDY-SCAG <scaggari@nmsu.edu>
2024-03-22 13:01:02 -0600

    1

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ app.use(express.static('CLIENT'));
 
 io.on('connection', (socket) => {
     console.log('user connected', socket.id);
-
+    socket.on('createGame', () => {
+        const newGameId = Math.floor(10000 + Math.random() * 90000).toString().substring(1);
+        console.log(newGameId);
+        games[newGameId] = {
+            currentPlayer: 'R',
+            gameId: newGameId,
+            gameOver: false
+        };
+        socket.join(newGameId);
+        socket.emit('gameCreated', newGameId);
+    });
+    console.log(newGameId
+        );
+    socket.on('gameAction', (data) => {
+        io.to(data.gameId).emit('gameUpdate', data);
+    })
     socket.on('disconnect', () => {
         console.log(
             'user disconnected: ', socket.id
@@ -22,4 +37,6 @@ io.on('connection', (socket) => {
 
 const PORT = 3000;
 
-server.listen(PORT, () => console.log('Server listening on Port: ${PORT}'));
+server.listen(PORT, () => { 
+    console.log('Server listening on Port: ${PORT}');
+});
